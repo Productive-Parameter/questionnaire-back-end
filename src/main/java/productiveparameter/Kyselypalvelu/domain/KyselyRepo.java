7cd9e0da586ddb7fbf5bab8f6a1135b9a2c77764
sampsale <82961425+sampsale@users.noreply.github.com>
2021-11-04 19:29:11 +0200

    Update KyselyRepo.java

--- src/main/java/productiveparameter/Kyselypalvelu/domain/KyselyRepo.java
+++ src/main/java/productiveparameter/Kyselypalvelu/domain/KyselyRepo.java
@@ -6,8 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-// @CrossOrigin("https://kyselypalvelu-ppfront.herokuapp.com/")
-// kommentoin tän toistaseks pois kun muuten ei voi laittaa pyyntöjä kotoa (vain herokusta) - sampsa
+@CrossOrigin("https://kyselypalvelu-ppfront.herokuapp.com/")
 public interface KyselyRepo extends CrudRepository<Kysely, Long>  {
 
     List<Kysely> findByNimi(String nimi);
