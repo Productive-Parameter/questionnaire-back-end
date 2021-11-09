3d5e4eecb735b09b0d556ff06cbe563c7deaf574
Riku Virtanen <virtanen.riku91@gmail.com>
2021-11-09 14:14:31 +0200

    crossorigin to controller

--- src/main/java/productiveparameter/Kyselypalvelu/domain/KyselyRepo.java
+++ src/main/java/productiveparameter/Kyselypalvelu/domain/KyselyRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-@CrossOrigin("*")
+@CrossOrigin
 public interface KyselyRepo extends CrudRepository<Kysely, Long>  {
 
     List<Kysely> findByNimi(String nimi);
