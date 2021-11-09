5026e9156969db6638dabecb1c362b5fb3aecb0a
tonifreden <toni.freden@gmail.com>
2021-11-09 14:57:09 +0200

    Kehitysvaatimuksia, pidemmät syöttökentät teksteille ja kyselyihin kuvausteksti

--- src/main/java/productiveparameter/Kyselypalvelu/domain/Kysely.java
+++ src/main/java/productiveparameter/Kyselypalvelu/domain/Kysely.java
@@ -20,6 +20,7 @@ public class Kysely {
     @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String nimi;
+	private String kuvaus;
 
 //Yhdellä teemalla on monia sanoja (wordmap)
 //Sanalla on yksi teema (tätä pitää kehittää sillä sanalla voi olla monta teemaa mutta ensin tehdään yksinkertaisemmin	
@@ -31,12 +32,20 @@ public class Kysely {
 	public Kysely() {
 		super();
 		this.nimi = null;
+		this.kuvaus = null;
 		this.kysymykset = null;
 	}
 	
 	public Kysely (String nimi, List<Kysymys> kysymykset) {
 		super();
 		this.nimi = nimi;
+		this.kysymykset = kysymykset;
+	}
+
+	public Kysely (String nimi, String kuvaus, List<Kysymys> kysymykset) {
+		super();
+		this.nimi = nimi;
+		this.kuvaus = kuvaus;
 		this.kysymykset = kysymykset;
 	}
 	
@@ -54,6 +63,14 @@ public class Kysely {
 	
 	public void setNimi(String nimi) {
 		this.nimi = nimi;
+	}
+
+	public String getKuvaus() {
+		return kuvaus;
+	}
+
+	public void setKuvaus(String kuvaus) {
+		this.kuvaus = kuvaus;
 	}
 
 	public List<Kysymys> getKysymykset() {
