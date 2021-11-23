73897d984e29806658d33d6e14cca19d783f54d9
Cho-ui <christian.lindell@myy.haaga-helia.fi>
2021-11-23 13:36:55 +0200

    Kyselyluokassa size-metodin jsonignore-annotaatio

--- src/main/java/productiveparameter/Kyselypalvelu/web/KysymysController.java
+++ src/main/java/productiveparameter/Kyselypalvelu/web/KysymysController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +19,7 @@ import productiveparameter.Kyselypalvelu.domain.Kysymys;
 import productiveparameter.Kyselypalvelu.domain.KysymysRepo;
 
 
-
+@CrossOrigin(origins = "*")
 @Controller
 public class KysymysController {
 	
