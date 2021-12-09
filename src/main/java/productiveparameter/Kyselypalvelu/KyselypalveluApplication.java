8fc5d3369f26436e2b067b12a68439115de00324
Riku Virtanen <virtanen.riku91@gmail.com>
2021-12-09 10:24:20 +0200

    fixed sec config and creted in memory users

--- src/main/java/productiveparameter/Kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/productiveparameter/Kyselypalvelu/KyselypalveluApplication.java
@@ -1,17 +1,24 @@
 package productiveparameter.Kyselypalvelu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import productiveparameter.Kyselypalvelu.domain.KyselyRepo;
 import productiveparameter.Kyselypalvelu.domain.KysymysRepo;
+import productiveparameter.Kyselypalvelu.web.UserDetailServiceImpl;
 
 @SpringBootApplication
 public class KyselypalveluApplication {
 	
 	// private static final Logger log = LoggerFactory.getLogger(KyselypalveluApplication.class);
+	@Autowired
+	private UserDetailServiceImpl userDetailsService;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyselypalveluApplication.class, args);
@@ -21,6 +28,12 @@ public class KyselypalveluApplication {
 	public CommandLineRunner demo(KyselyRepo kyselyRepo, KysymysRepo kysymysRepo) {
 		return  (args) -> {	
 		};
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	 auth.userDetailsService(userDetailsService).passwordEncoder(new
+	 BCryptPasswordEncoder());
 	}
 
 }
--- src/main/java/productiveparameter/Kyselypalvelu/WebSecurityConfig.java
+++ src/main/java/productiveparameter/Kyselypalvelu/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package productiveparameter.Kyselypalvelu;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 // import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -24,18 +28,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .authorizeRequests().antMatchers("/api/**").permitAll() // Katsotaan tarvitaanko tätä tai tuleeko tähän jotain lisää tai muuta tilalle
             .and()
+            .headers().frameOptions().sameOrigin()
+            .and()
             .authorizeRequests().anyRequest().authenticated()
             .and()
         .formLogin()
-            .loginPage("/login")
-            .defaultSuccessUrl("/", true).permitAll()
+            .defaultSuccessUrl("/", true)
+            .permitAll()
             .and()
         .logout()
-            .permitAll();
+	        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/login");
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         // auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+    
+    @Bean 
+    public PasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder(); 
+    }
+    
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+            .and()
+            .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
     }
 }
